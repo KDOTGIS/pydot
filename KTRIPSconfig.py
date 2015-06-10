0d5eef40e15b3d8f87650980b4c100aebe8ba9db
KDOT_GIS <kyleg@ksdot.org>
2015-06-10 17:06:45 -0500

    adding the NG911_KANSAS_DOT project, with udpates designed to perform processing and aggregation on all the NG911 file geodatabase datasets in a single directory

---
+++ KTRIPSconfig.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        KTRIPS_Config
+# Purpose:
+#
+# Author:      kyleg
+#
+# Created:     04/21/2015
+# Copyright:   (c) kyleg 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+repo = r"\\gisdata\arcgis\GISdata\KDOT\DESIGN\KTRIPS\V1"
+cansys = r"C:\Workspace\pydot\sde_connections_10.3\RO@sqlgisprod_GIS_cansys.sde\GIS_CANSYS.SHARED.CMLRS"
+gdb = r"C:\Workspace\pydot\sde_connections_10.3\sde@KTRIPS_sqlgisprod.sde"
+#monthdb = r"C:\Workspace\pydot\sde_connections_10.3\sde@KTRIPS_sqlgisprod.sde\KTRIPS.SDE.Routes"
+db = r"KTRIPS"
+schema = r"sde"
